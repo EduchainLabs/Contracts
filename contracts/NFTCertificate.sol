4483ec1f715df667799b943879fb395309362a5e
raksha-bv <rakshabv.work@gmail.com>
2025-04-07 17:48:40 +0530

    fix: NFT certificate contract without certificates in contract

--- contracts/NFTCertificate.sol
+++ contracts/NFTCertificate.sol
@@ -3,36 +3,27 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+
 
 contract NFTCertificate is ERC721, Ownable {
 
-    using Strings for uint256;
-    string[] public nftMetadataURIs = [
-        "https://aqua-rare-worm-454.mypinata.cloud/ipfs/bafybeiecowelxaity7cm5vo7uiy2q3xj2vv6wyhsa4w7yglyhvxakfylw4/brain.json",
-        "https://aqua-rare-worm-454.mypinata.cloud/ipfs/bafybeiecowelxaity7cm5vo7uiy2q3xj2vv6wyhsa4w7yglyhvxakfylw4/speed.json"
-    ];
-
     uint256 public nextTokenId;
-    mapping(uint256 => uint256) private tokenIdToMetadataIndex;
+    mapping(uint256 => string) private tokenIdToMetadataURI;
 
     event CertificateMinted(address recipient, uint256 tokenId, string metadataURI);
 
     constructor() ERC721("NFTCertificate", "CERT") Ownable(msg.sender) {}
 
-    function mintCertificate(address recipient, uint256 metadataIndex) external {
-        require(metadataIndex < nftMetadataURIs.length, "Invalid metadata index");
-
+    function mintCertificate(address recipient, string memory metadataURI) external {
         uint256 tokenId = nextTokenId;
         _safeMint(recipient, tokenId);
-        tokenIdToMetadataIndex[tokenId] = metadataIndex;
+        tokenIdToMetadataURI[tokenId] = metadataURI;
         nextTokenId++;
 
-        emit CertificateMinted(recipient, tokenId, nftMetadataURIs[metadataIndex]);
+        emit CertificateMinted(recipient, tokenId, metadataURI);
+    }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return tokenIdToMetadataURI[tokenId];
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-        uint256 metadataIndex = tokenIdToMetadataIndex[tokenId]; 
-        return nftMetadataURIs[metadataIndex];
-    }
 }
